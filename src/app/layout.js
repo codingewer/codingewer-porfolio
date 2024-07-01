36de65efc30bb6ad218eba3d381ff6cee32d81ef
codingewer <yatli5645@gmail.com>
2024-07-01 22:37:10 +0300

    some bad words

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,11 +7,15 @@ import "../styles/pages.css"
 export const metadata = {
   title: "codingewer",
   description: "AI and Software",
+
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="./favicon.ico" />
+      </head>
       <body className="App">{children}</body>
     </html>
   );
