5e4400b6ca92bb7bc2438e79a46d7c11984edd2d
codingewer <yatli5645@gmail.com>
2024-07-01 23:00:42 +0300

    sdsd

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,20 @@
 import "./globals.css";
-import "../styles/topbar.css"
-import "../styles/home.css"
-import "../styles/pages.css"
-
+import "../styles/topbar.css";
+import "../styles/home.css";
+import "../styles/pages.css";
+import Head from "next/head";
 
 export const metadata = {
   title: "codingewer",
   description: "AI and Software",
-
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-      <link rel="icon" href="./favicon.ico" />
-      </head>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className="App">{children}</body>
     </html>
   );
